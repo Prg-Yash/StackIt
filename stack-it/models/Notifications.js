3734551b57e42b8dd75fe8a1d63877ff1a525dfd
Aryan shinde <shindearyan179@gmail.com>
2025-07-12 13:51:32 +0530

    Added notification api route and model

---
+++ stack-it/models/Notifications.js
@@ -0,0 +1,34 @@
+// /models/Notification.js
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const NotificationSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ['new_answer', 'question_upvote', 'answer_upvote'],
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Notification ||
+  mongoose.model('Notification', NotificationSchema);
